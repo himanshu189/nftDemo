27b4933a02694db8aa9cfd418b33a52dcdfb4c87
Himanshu Tyagi <you@example.com>
2021-11-23 14:10:48 +0530

    dhrdd

---
+++ BackendOfNft/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+
+const userRoute = require("./routes/user");
+
+// const mongoURI = "mongodb://localhost:27017/BlockchainPOC";
+
+//connnect to DB
+
+mongoose
+  .connect(process.env.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true, // not supported
+  })
+  .then((result) => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => console.log(err));
+
+//middlewares
+app.use(bodyParser.json());
+app.use("/api/user", userRoute);
+
+app.listen(8080, () => console.log("Server running at 8080..."));
